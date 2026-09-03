8ce8e64b6f9d41a0db4cfe707a8ee59625d9688f
marketing-pixel <marketing@cortezmoveis.com.br>
2026-09-03 13:49:46 -0300

    feat: integrate cloudinary image storage

---
+++ src/config/cloudinary.js
@@ -0,0 +1,17 @@
+const { v2: cloudinary } = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+function cloudinaryConfigurado() {
+    return Boolean(
+        process.env.CLOUDINARY_CLOUD_NAME &&
+        process.env.CLOUDINARY_API_KEY &&
+        process.env.CLOUDINARY_API_SECRET
+    );
+}
+
+module.exports = { cloudinary, cloudinaryConfigurado };
